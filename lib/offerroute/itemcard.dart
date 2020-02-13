9648eec0f44dcaa01c3d42e5bf69ffa4d6006939
Tim <lampentimothy42@gmail.com>
2020-02-13 10:58:10 -0500

    added listing screen

--- lib/offerroute/itemcard.dart
+++ lib/offerroute/itemcard.dart
@@ -1,20 +1,74 @@
+import 'dart:ui';
+
 import 'package:flutter/material.dart';
 
 import 'listing.dart';
 
-class ItemCard extends StatelessWidget {
-  final Listing listing;
+class ItemCard extends StatefulWidget {
+  final Listing _listing;
 
-  ItemCard(this.listing);
+  ItemCard(this._listing);
+
+
+  @override
+  _ItemCardState createState() => _ItemCardState(this._listing);
+}
+
+class _ItemCardState extends State<ItemCard> {
+  final Listing _listing;
+
+  _ItemCardState(this._listing);
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      decoration: BoxDecoration(
-          borderRadius: BorderRadius.all(Radius.circular(15)),
-          color: Colors.red),
-      child: Text(listing.description),
-      margin: EdgeInsets.only(top: 20),
+    return InkWell(
+      onHover: (e) {
+
+      },
+      child: Card(
+        margin: EdgeInsets.all(14),
+        color: Colors.blue[900],
+        child: Container(
+          decoration: new BoxDecoration(
+              border: new Border.all(
+                color: Colors.lightBlueAccent,
+                width: 3.0,
+                style: BorderStyle.solid,
+              ),
+              borderRadius: BorderRadius.all(Radius.circular(15))),
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: <Widget>[
+              ListTile(
+                leading: Icon(
+                  this._listing.icon,
+                  color: Colors.white,
+                ),
+                title: Text(
+                  this._listing.title,
+                  style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      color: Colors.lightBlueAccent,
+                      fontSize: 18),
+                ),
+                subtitle: Text(
+                  this._listing.description,
+                  style: TextStyle(color: Colors.lightBlue[100]),
+                ),
+              ),
+              ButtonBar(
+                alignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  FlatButton(
+                    child: Text("ADD TO CART"),
+                    onPressed: () {},
+                  )
+                ],
+              )
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
--- lib/offerroute/listings.dart
+++ lib/offerroute/listings.dart
@@ -12,30 +12,72 @@ class _State extends State<Listings> {
   final exampleLists = [
     Listing(
         title: "64L Keg of Belgium Moon",
-        description: "Every party needs some liquid courage to loosen up the that inital awkwardness feeling. Why not kick off the party right with an entire keg? After a keg stand or two, the party will be bumping!",
+        description:
+            "Every party needs some liquid courage to loosen up the that inital awkwardness feeling. Why not kick off the party right with an entire keg? After a keg stand or two, the party will be bumping!",
         price: 250,
         icon: Icons.hourglass_full),
     Listing(
         title: "24x Palette of Beer",
-        description: "Sometimes you want to be able to split up the party a bit more and not get keg-ice all over the floor. A palette of beer is a good middle ground.",
+        description:
+            "Sometimes you want to be able to split up the party a bit more and not get keg-ice all over the floor. A palette of beer is a good middle ground.",
         price: 150,
         icon: Icons.android),
     Listing(
         title: "1x Foldable Table",
-        description: "Every party needs some liquid courage to loosen up the that inital awkwardness feeling. Why not kick off the party right with an entire keg? After a keg stand or two, the party will be bumping!",
+        description:
+            "Yes, someone will jump on this table for a CPL video but don't worry since they're so cheap!",
+        price: 75,
+        icon: Icons.keyboard_tab),
+    Listing(
+        title: "1x Foldable Table",
+        description:
+            "Yes, someone will jump on this table for a CPL video but don't worry since they're so cheap!",
+        price: 75,
+        icon: Icons.keyboard_tab),
+    Listing(
+        title: "1x Foldable Table",
+        description:
+            "Yes, someone will jump on this table for a CPL video but don't worry since they're so cheap!",
+        price: 75,
+        icon: Icons.keyboard_tab),
+    Listing(
+        title: "1x Foldable Table",
+        description:
+            "Yes, someone will jump on this table for a CPL video but don't worry since they're so cheap!",
         price: 75,
         icon: Icons.keyboard_tab),
   ];
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: <Widget>[
-        Text()
-        ...exampleLists.map((listing) {
-          return ItemCard(listing);
-        })
-      ],
-    );
+    return Scaffold(
+        backgroundColor: Colors.blue[900],
+        body: Container(
+            margin: EdgeInsets.only(top: 30),
+            child: ListView(
+              children: <Widget>[
+                Container(
+                  child: Center(
+                    child: ListTile(
+                      title: Text(
+                        "Pick Your Poison",
+                        textAlign: TextAlign.center,
+                        style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          color: Colors.lightBlueAccent,
+                          fontSize: 36,
+                        ),
+                      ),
+                      subtitle: Text(
+                        "Select the items that you would like to add to your cart and they will be delivered to you within 2 hours.",
+                        textAlign: TextAlign.center,
+                        style: TextStyle(color: Colors.lightBlue[100]),
+                      ),
+                    ),
+                  ),
+                ),
+                ...(exampleLists).map((e) => ItemCard(e))
+              ],
+            )));
   }
 }
